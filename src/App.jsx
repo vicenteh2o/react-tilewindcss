819b868f8dc8f563e36bc979175c5afb20bb7852
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-05-16 11:44:34 -0300

    feat: about section done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import LoadingScreen from "./components/LoadingScreen";
 import Navbar from "./components/Navbar";
 import MobileMenu from "./components/MobileMenu";
 import Home from "./components/sections/Home";
+import About from "./components/sections/About";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -23,6 +24,7 @@ function App() {
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
+        <About />
       </div>
     </>
   );
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -5,12 +5,12 @@ const Home = () => {
       className="min-h-screen flex items-center justify-center relative"
     >
       <div className="text-center z-10 px-4">
-        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
           Hola, soy Vicente Fleitas
         </h1>
 
         <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
-          Soy desarrollador fullstack, me gusta el clean code, aplicaciones web
+          Desarrollador fullstack, me gusta el clean code, aplicaciones web
           escalables. Mi objetivo es crear soluciones que ofrezcan un buen
           performance y una experiencia de usuario agradable.
         </p>
