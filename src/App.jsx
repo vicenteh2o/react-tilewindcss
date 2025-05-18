2cee72b206d1e78031906010b2e6f3fb614cd6ed
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-05-18 19:11:23 -0300

    feat: contact screen and email

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import MobileMenu from "./components/MobileMenu";
 import Home from "./components/sections/Home";
 import About from "./components/sections/About";
 import Projects from "./components/sections/Projects";
+import Contact from "./components/sections/Contact";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -27,6 +28,7 @@ function App() {
         <Home />
         <About />
         <Projects />
+        <Contact />
       </div>
     </>
   );
---
+++ src/components/sections/Contact.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import RevealOnScroll from "../RevealOnScroll";
+import emailjs from "emailjs-com";
+
+const Contact = () => {
+  const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
+  const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
+  const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
+      .then((result) => {
+        alert("Mensaje Enviado!");
+        setFormData({ name: "", email: "", message: "" });
+      })
+      .catch(() => alert("Algo salio mal. Intenta de nuevo."));
+  };
+  return (
+    <section
+      id="contact"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="px-4 w-150">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            Contactar
+          </h2>
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div className="relative">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                required
+                value={formData.name}
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition 
+                            focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="Name..."
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+              />
+            </div>
+
+            <div className="relative">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                value={formData.email}
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition 
+                            focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="example@gmail.com"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+              />
+            </div>
+
+            <div className="relative">
+              <textarea
+                id="message"
+                name="message"
+                required
+                value={formData.message}
+                rows={5}
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition 
+                            focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="Your message..."
+                onChange={(e) =>
+                  setFormData({ ...formData, message: e.target.value })
+                }
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium 
+                                            transition relative overflow-hidden
+                                            hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]
+            "
+            >
+              Enviar Mensaje
+            </button>
+          </form>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
+
+export default Contact;
